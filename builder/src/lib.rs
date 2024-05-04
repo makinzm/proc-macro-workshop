08e4af2f6f5b0202be12f1fb8e5f68db8b7b6efb
makinzm <nozomi.maki.da@gmail.com>
2024-05-04 15:32:17 +0900

    feat: 🎸 test 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,19 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = "hello";
+    
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote! {
+        let #name = 10;
+    };
+
+    // Hand the output tokens back to the compiler
+    expanded.into()
 }
--- main.rs
+++ main.rs
@@ -6,4 +6,16 @@
 // To run the code:
 //     $ cargo run
 
+// For Debugging purposes
+
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
 fn main() {}
