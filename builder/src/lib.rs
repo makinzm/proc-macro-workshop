d93acc5e1cbf7072f445164007a78c734fefc09f
takesei <sei06k14@gmail.com>
2024-05-05 22:02:46 +0900

    :tada: solve quiz4

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -17,15 +17,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn derive_builder(input: DeriveInput) -> Result<TokenStream, &'static str> {
     // deriveInput must have ident and data
     // https://docs.rs/syn/latest/syn/struct.Ident.html
-    let input_ident = &input.ident;
+    let src_name = &input.ident;
     // https://docs.rs/syn/latest/syn/enum.Data.html
-    let input_data = &input.data;
+    let src_data = &input.data;
 
-    let ident_builder = quote::format_ident!("{}Builder", input_ident);
+    let builder_name = quote::format_ident!("{}Builder", src_name);
 
     // https://docs.rs/syn/latest/syn/struct.FieldsNamed.html
     // &syn::punctuated::Punctuated<syn::Field, syn::token::Comma>
-    let fields_named = match input_data {
+    let fields_named = match src_data {
         syn::Data::Struct(syn::DataStruct {
             fields: syn::Fields::Named(syn::FieldsNamed { named: val, .. }),
             ..
@@ -35,7 +35,9 @@ fn derive_builder(input: DeriveInput) -> Result<TokenStream, &'static str> {
 
     let mut fields = Vec::new();
     let mut fields_init = Vec::new();
-    let mut field_setter = Vec::new();
+    let mut fields_setter = Vec::new();
+
+    let mut struct_element = Vec::new();
 
     // Punctuated<T, P> iter works for T not P
     // https://docs.rs/syn/latest/src/syn/punctuated.rs.html#96-103
@@ -46,22 +48,32 @@ fn derive_builder(input: DeriveInput) -> Result<TokenStream, &'static str> {
 
         fields.push(create_field(ident, ty));
         fields_init.push(create_init(ident));
-        field_setter.push(create_setter(ident, ty));
+        fields_setter.push(create_setter(ident, ty));
+
+        struct_element.push(create_struct_element(ident));
     }
 
     // Build the output, possibly using quasi-quotation
     let expanded = quote! {
-        pub struct #ident_builder {
+        pub struct #builder_name {
             #(#fields)*
         }
 
-        impl #ident_builder {
-            #(#field_setter)*
+        impl #builder_name {
+            #(#fields_setter)*
+
+            pub fn build(&mut self) -> Result<#src_name, &'static str> {
+                Ok(
+                    #src_name {
+                        #(#struct_element)*
+                    }
+                )
+            }
         }
 
-        impl #input_ident {
-            pub fn builder() -> #ident_builder {
-                #ident_builder {
+        impl #src_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
                     // https://github.com/dtolnay/quote?tab=readme-ov-file#repetition
                     #(#fields_init)*
                 }
@@ -84,11 +96,18 @@ fn create_init(ident: &syn::Ident) -> TokenStream2 {
         #ident: None,
     }
 }
+
 fn create_setter(ident: &syn::Ident, ty: &syn::Type) -> TokenStream2 {
     quote! {
         fn #ident(&mut self, #ident: #ty) -> &mut Self {
             self.#ident = Some(#ident);
             self
         }
+    }
+}
+
+fn create_struct_element(ident: &syn::Ident) -> TokenStream2 {
+    quote! {
+        #ident: self.#ident.clone().unwrap(),
     }
 }
